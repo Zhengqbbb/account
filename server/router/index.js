2e64aa0370d96e1bee8814e063a4a1b710370767
zhengqbbb <1074059947@qq.com>
2020-03-30 01:01:00 +0800

    :construction: Done Serve interface

---
+++ server/router/index.js
@@ -0,0 +1,60 @@
+module.exports = app => {
+  const express = require('express');
+  const router = express.Router()
+  const fs = require('fs')
+  const path = require('path')
+
+  /**
+   * @description: 将表格型数据转换为对象数组格式
+   * @param {tableHead: []} :表头
+   * @param {table: []} :整个表格
+   * @return: [] : 数组对象
+   */
+  const tableToArrayObject = function(tableHead, table) {
+    const arr = [];
+    table.shift();
+    for (let i = 0; i < table.length; i++) {
+      const hash = {};
+      for (let j = 0; j < table[i].length; j++) {
+        for (let k = 0; k < tableHead.length; k++) {
+          if (k === j) {
+            const value = table[i][j]
+            hash[tableHead[k]] = value
+          }
+        }
+      }
+      arr.push(hash)
+    }
+    return arr
+  }
+  router.get('/bills', async (req, res) => {
+    const billPath = path.resolve(__dirname, './../static/bill.csv');
+    fs.readFile(billPath, function(err, fileData) {
+      if (err) console.log(err);
+      fileData = fileData.toString();
+      var table = new Array();
+      var rows = fileData.split("\r\n");
+      for (var i = 0; i < rows.length; i++) {
+        table.push(rows[i].split(","));
+      }
+      const data = tableToArrayObject(table[0], table)
+      res.send(data)
+    })
+  })
+
+  router.get('/categories', async (req, res) => {
+    const categoriesPath = path.resolve(__dirname, './../static/categories.csv');
+    fs.readFile(categoriesPath, function(err, fileData) {
+      if (err) console.log(err);
+      fileData = fileData.toString();
+      const table = new Array();
+      const rows = fileData.split("\r\n");
+      for (var i = 0; i < rows.length; i++) {
+        table.push(rows[i].split(","));
+      }
+      const data = tableToArrayObject(table[0], table)
+      res.send(data)
+    })
+  })
+  app.use('/api', router)
+}
